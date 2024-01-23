7bf7698348382c1355b93a85387b5a0f35432108
priyanshi chauhan <priyanshi.chauhan@opstree.com>
2024-01-23 14:23:11 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
 FROM ubuntu
 RUN apt update && apt install jq -y && apt install git -y
+RUN apt-get update && \
+    apt-get install -y bash curl python3 python3-pip && \
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
+    python3 get-pip.py && \
+    pip install cryptography
 COPY build.sh .
 COPY BP-BASE-SHELL-STEPS .
 RUN chmod +x build.sh
-ENV ACTIVITY_SUB_TASK_CODE BP-ECR-REPO-CREATION-TASK
+ENV ACTIVITY_SUB_TASK_CODE BP-GIT-TAG-CREATE-TASK
 ENV SLEEP_DURATION 0s
 ENV TAG_NAME ""
 ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -10,11 +10,22 @@ source aws-functions.sh
 logInfoMessage "I'll create a Git tag for a branch if it doesn't exist."
 sleep $SLEEP_DURATION
 
+ENCRYPTED_CREDENTIAL_USERNAME=$(getEncryptedCredential "$GIT_REPO" "GIT_INFO.GIT_USERNAME")
+CREDENTIAL_USERNAME=$(getDecryptedCredential "$FERNET_KEY" "$ENCRYPTED_CREDENTIAL_USERNAME")
+
+
+ENCRYPTED_CREDENTIAL_PASSWORD=$(getEncryptedCredential "$GIT_REPO" "GIT_INFO.GIT_PASSWORD")
+CREDENTIAL_PASSWORD=$(getDecryptedCredential "$FERNET_KEY" "$ENCRYPTED_CREDENTIAL_PASSWORD")
+
+
 # Check if the branch name and tag name are provided
 if [[ -z "$TAG_NAME" ]]; then
   logErrorMessage "Please provide the tag name."
   exit 1
 fi
+
+GIT_URL=$(getEncryptedCredential "$GIT_REPO" "GIT_INFO.GIT_URL")
+GIT_BRANCH=$(getEncryptedCredential "$GIT_REPO" "GIT_INFO.GIT_BRANCH")
 
 # Extract the repository name
 REPO_NAME=$(basename "$GIT_URL" ".git")
@@ -39,7 +50,8 @@ cd $REPO_NAME
   git checkout $GIT_BRANCH > /dev/null 2>&1
 
   if git tag -l "$TAG_NAME" | grep -q "$TAG_NAME"; then
-    logInfoMessage "Git tag $TAG_NAME already exists in repository [$REPO_NAME]"
+    logErrorMessage "Git tag $TAG_NAME already exists in repository [$REPO_NAME]"
+    exit 1
   else
     # Create the Git tag
     git tag $TAG_NAME 
